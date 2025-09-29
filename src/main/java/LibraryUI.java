8afb3f8f7ee8008cf8b86cc5f67e3c16093e764e
TimXia7 <timxia6208@gmail.com>
2025-09-28 21:11:09 -0400

    R-CODE-03

--- src/main/java/LibraryUI.java
+++ src/main/java/LibraryUI.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class LibraryUI {
     Library library;
@@ -6,5 +7,19 @@ public class LibraryUI {
         this.library = library;
     }
 
-    public void promptCredentials(){}
+    public String[] promptCredentials() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Clear the terminal
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        return new String[] { username, password };
+    }
 }
